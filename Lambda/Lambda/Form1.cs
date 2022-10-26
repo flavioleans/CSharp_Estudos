4561a83c391a511c70c677c5dca0a24fe6047e5a
flavioleans <leans.flavio@gmail.com>
2022-10-26 10:14:36 -0300

    lambda e cilindro

---
+++ Lambda/Lambda/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lambda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            //expressão lambda que tem uma expressão como corpo:
+            //(input - parameters) => expression
+
+            //instrução lambda que tem um bloco de instrução como corpo:
+            //(input - parameters) => { < sequence - of - statements > }
+
+            //sem lambda
+            //Func<int, int> square = quadrado;
+            //labelResultado.Text = "o resultado é: " + square(5);
+
+            //com lambda
+            Func<int, int> square = x => x * x;
+            //labelResultado.Text = "o resultado é: " + square(5);
+
+            Expression<Func<int, int>> ex = x => x + x;
+            //labelResultado.Text = ex.ToString();
+
+            //lambda com link
+            int[] numbers = { 2, 3, 4, 5 };
+            var squareNumbers = numbers.Select(x => x * x);
+            //labelResultado.Text = string.Join(" ", squareNumbers);
+
+            //Action<string> greet = name =>
+            //{
+            //    string greeting = $"Hello {name}!";
+            //    labelResultado.Text = greeting;
+            //};
+
+            //greet("Flavio");
+
+            //-----------------------------------------
+            //Action line = () => Console.WriteLine();
+            //Console.WriteLine("Flavio ");
+            //line();
+            //Console.WriteLine("Leandro");
+
+            Func<int, int, bool> testForEquality = (x, y) => x == y;
+            labelResultado.Text = testForEquality(10,10).ToString();
+            
+        }
+
+        int quadrado(int x)
+        {
+            return x * x;
+        }
+    }
+}
---
+++ VolumeCilindro/VolumeCilindro/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VolumeCilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, A, H, V;
+            R = 0;
+            while (R <= 0)
+            {
+                Console.WriteLine("Digite o valor do raio da base do cilindro: ");
+
+                R = double.Parse(Console.ReadLine());
+            }
+
+            H = 0;
+            while (H <= 0)
+            {
+                Console.WriteLine("Digite o valor da altura do cilindro: ");
+                H = double.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine();
+            //area da base
+            A = Math.PI * Math.Pow(R, 2);
+
+            //volume do cilindro
+            V = A * H;
+
+            Console.WriteLine($"O volume do cilindro é de {V}");
+        }
+    }
+}
